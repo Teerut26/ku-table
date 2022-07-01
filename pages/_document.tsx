74866675a742b19fad7cd02d7422b5c18f5facda
Teerut26 <teerutsr@gmail.com>
2022-07-01 09:06:09 +0700

    add meta tag for better seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,36 @@ export default function Document() {
         <Html data-theme="light">
             <Head>
                 <title>KU Table 2</title>
-                <meta name="nextjs-typescript-tailwind-redux-graphql" />
+                <meta name="title" content="KU Table 2" />
+                <meta
+                    name="description"
+                    content="ตรวจสอบตารางเรียน เด็กเกษตรศาสตร์ ทุกวิทยาเขต (บางเขน กำแพงแสน ศรีราชา) สามารถบันทึกรูปตารางเรียนได้ เพียงล็อกอินด้วยบัญชีนนทรีเท่านั้น!"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://table.ku-cs.com/" />
+                <meta property="og:title" content="KU Table 2" />
+                <meta
+                    property="og:description"
+                    content="ตรวจสอบตารางเรียน เด็กเกษตรศาสตร์ ทุกวิทยาเขต (บางเขน กำแพงแสน ศรีราชา) สามารถบันทึกรูปตารางเรียนได้ เพียงล็อกอินด้วยบัญชีนนทรีเท่านั้น!"
+                />
+                <meta
+                    property="og:image"
+                    content="https://firebasestorage.googleapis.com/v0/b/ku-table.appspot.com/o/image%2Fkutable-726bfa0e-a48a-4ec2-a55a-8f10bc2b4a9a.png?alt=media&token=68336326-152f-4700-9e15-7cd89ffe0c80"
+                />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta
+                    property="twitter:url"
+                    content="https://table.ku-cs.com/"
+                />
+                <meta property="twitter:title" content="KU Table 2" />
+                <meta
+                    property="twitter:description"
+                    content="ตรวจสอบตารางเรียน เด็กเกษตรศาสตร์ ทุกวิทยาเขต (บางเขน กำแพงแสน ศรีราชา) สามารถบันทึกรูปตารางเรียนได้ เพียงล็อกอินด้วยบัญชีนนทรีเท่านั้น!"
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://firebasestorage.googleapis.com/v0/b/ku-table.appspot.com/o/image%2Fkutable-726bfa0e-a48a-4ec2-a55a-8f10bc2b4a9a.png?alt=media&token=68336326-152f-4700-9e15-7cd89ffe0c80"
+                />
                 <link rel="icon" href="/logo.svg" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
