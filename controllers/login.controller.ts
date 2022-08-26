000221214e6f38674f2354c17573a8f4e895d4c8
Teerut26 <teerutsr@gmail.com>
2022-08-26 12:42:27 +0700

    fix login

--- controllers/login.controller.ts
+++ controllers/login.controller.ts
@@ -1,13 +1,27 @@
 import catchErrorsFrom from "libs/_utils/errors";
 import { NextApiRequest, NextApiResponse } from "next";
 import AxiosServiceBackend from "services/backend/axios.service";
+ 
+import crypto from "crypto"
+
+const encodeString = (str: string) => {
+    return crypto
+        .publicEncrypt(
+            {
+                key: process.env.PUBLIC_KEY!,
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            },
+            Buffer.from(str, "utf8")
+        )
+        .toString("base64");
+};
 
 const Login = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         let axiosService = new AxiosServiceBackend();
         let { data } = await axiosService.login(
-            req.body.username,
-            req.body.password
+            encodeString(req.body.username),
+            encodeString(req.body.password)
         );
         res.json(data)
     } catch (error:any) {
